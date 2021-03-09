a9ccf21f4a1e20f5c675ad8d551bc1e04bb5263a
Bryan Teng <bteng1226@gmail.com>
2021-03-09 17:36:32 -0600

    Add project files.

---
+++ XamarinNotesApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XamarinNotesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public IList<Note> Notes { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Notes = new List<Note>();
+            Notes.Add(new Note
+            {
+                Title = "Chores",
+                Detail = "Dishes and Laundry",
+            });
+
+            Notes.Add(new Note
+            {
+                Title = "Homework",
+                Detail = "Math and science",
+            });
+
+            BindingContext = this;
+        }
+        void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Note selectedItem = e.CurrentSelection[0] as Note;
+        }
+    }
+}
---
+++ XamarinNotesApp/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinNotesApp
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
